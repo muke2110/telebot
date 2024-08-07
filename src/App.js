102580714bf201c1882ee485621555b8e3d030b3
muke2110 <muke2110@gmail.com>
2024-08-07 22:32:37 +0530

    Spin update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import './App.css'
-import WheelComponent from './wheelComponent';
+import React from "react";
+import "./App.css";
+import WheelComponent from "react-wheel-of-prizes";
 
 export default function App() {
+  const segments = [
+    "better luck next time",
+    "won 70",
+    "won 10",
+    "better luck next time",
+    "won 2",
+    "won uber pass"
+  ];
+  const segColors = ["#EE4040", "#F0CF50", "#815CD1", "#3DA5E0", "#34A24F"];
+  const onFinished = (winner) => {
+    console.log(winner);
+  };
 
   return (
     <div className="App">
-      <div>
-        <WheelComponent />
+      <h1>Spin the Wheel!</h1>
+      <div className="wheel-container">
+        <WheelComponent
+          segments={segments}
+          segColors={segColors}
+          onFinished={(winner) => onFinished(winner)}
+          primaryColor="black"
+          contrastColor="white"
+          buttonText="Spin"
+          isOnlyOnce={false}
+          size={190}
+          upDuration={500}
+          downDuration={600}
+          fontFamily="Arial"
+        />
       </div>
+      <h2>Good Luck!</h2>
     </div>
   );
 }
